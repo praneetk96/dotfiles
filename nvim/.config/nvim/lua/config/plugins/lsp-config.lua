f904f83f0df27faa60dc520467a975992284f6e5
Praneet Kumar <praneetk96@gmail.com>
2026-03-02 16:01:01 +0530

    Comment out LSP server configurations
    Comment out LSP server setups for TypeScript, Solargraph, HTML, and Lua.

--- nvim/.config/nvim/lua/config/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp-config.lua
@@ -20,18 +20,18 @@ return {
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
             local lspconfig = vim.lsp.config
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.solargraph.setup({
-                capabilities = capabilities
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
+            -- lspconfig.ts_ls.setup({
+            --     capabilities = capabilities
+            -- })
+            -- lspconfig.solargraph.setup({
+            --     capabilities = capabilities
+            -- })
+            -- lspconfig.html.setup({
+            --     capabilities = capabilities
+            -- })
+            -- lspconfig.lua_ls.setup({
+            --     capabilities = capabilities
+            -- })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
