69e862db2e305fc8c4e867c018b2d3d4640dbd69
Praneet Kumar <praneetk96@gmail.com>
2026-03-02 15:44:18 +0530

    Fix LSP configuration for changes in new neovim

--- nvim/.config/nvim/lua/config/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp-config.lua
@@ -19,8 +19,7 @@ return {
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            --lspconfig.tsserver.setup({
+            local lspconfig = vim.lsp.config
             lspconfig.ts_ls.setup({
                 capabilities = capabilities
             })
